e80a2e1b50838ada0ed6406c55fc6ec71b5487d4
Taco <taco-tues@tuta.io>
2020-01-21 23:09:29 -0700

    Create Card component

---
+++ app/components/Card/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import imageSwitch from '../../utils/imageSwitch';
+import Modal from '../Modal/Modal';
+import useModal from '../../hooks/useModal';
+
+export default function Card({ type, title, position }) {
+  const {isShowing, toggle} = useModal();
+  
+  return (
+    <div className="card" key={position}>
+      <img onClick={toggle} className="img" variant="top" src={imageSwitch(type)} />
+      <Modal
+        isShowing={isShowing}
+        hide={toggle}
+        imageSource={imageSwitch(type)}
+      />
+      <div className="card card-body">
+        <div className="card center-text">{title}</div>
+      </div>
+    </div>
+  );
+}
