27a342a2437b907c994cc9ffc0fba22c9c7434c7
Taco <taco-tues@tuta.io>
2020-01-21 17:10:10 -0700

    Create index.js

---
+++ app/index.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import "./index.css";
+import { addIndex } from './utils/helpers.js'
+import DragItem from "./components/DragItem/DragItem";
+import DropItem from "./components/DropItem/DropItem";
+import ReactDom from 'react-dom';
+import staticJSON from '../resources/json/cat-data.json';
+import util from 'util';
+
+
+function App() {
+  const [cardValues, setCardValue] = useState(addIndex(staticJSON));
+
+  return (
+    <div className="container">
+      <div className="box">
+        <DropItem
+          heading="Slinky Cats"
+          onDrop={id => {
+            const currentCard = { ...cardValues[id] };
+            currentCard.position = 0;
+            setCardValue({ ...cardValues, ...{ [id]: currentCard } });
+          }}
+        >
+          {Object.keys(cardValues)
+            .map(key => ({ id: key, ...cardValues[key] }))
+            .filter(card => card.position <= 1)
+            .map(card => <DragItem key={card.id} id={card.id} data={card}  />)}
+        </DropItem>
+        <DropItem
+          heading="Jedi Cats"
+          onDrop={id => {
+            const currentCard = { ...cardValues[id] };
+            currentCard.position = 2;
+            setCardValue({ ...cardValues, ...{ [id]: currentCard } });
+          }}
+        >
+          {Object.keys(cardValues)
+            .map(key => ({ id: key, ...cardValues[key] }))
+            .filter(card => card.position > 1 && card.position <= 3)
+            .map(card => <DragItem id={card.id} data={card} key={card.id} />)}
+        </DropItem>
+        <DropItem
+          heading="Fancy Cats"
+          onDrop={id => {
+            const currentCard = { ...cardValues[id] };
+            currentCard.position = 4;
+            setCardValue({ ...cardValues, ...{ [id]: currentCard } })
+          }}
+        >
+          {Object.keys(cardValues)
+            .map(key => ({ id: key, ...cardValues[key] }))
+            .filter(card => card.position >= 4)
+            .map(card => <DragItem id={card.id} data={card} key={card.id} />)}
+        </DropItem>
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDom.render(<App />, rootElement);
