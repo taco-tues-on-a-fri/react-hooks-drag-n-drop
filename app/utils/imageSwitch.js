865815f199429b371c6887eb566c26a5ba971f04
Taco <taco-tues@tuta.io>
2020-01-21 17:02:35 -0700

    Create imageSwitch.js

---
+++ app/utils/imageSwitch.js
@@ -0,0 +1,21 @@
+//| Switch statement to select what gif to return based off of type
+//|------------------------------------------------------------------------
+export default function imageSwitch(param) {
+  const path = '../resources/gif/'
+  switch(param) {
+    case 'slinky-cat-01':
+      return `${path}shaq-cat.gif`;
+    case 'slinky-cat-02':
+      return `${path}pizza-taco-cat.gif`;
+    case 'jedi-cat-01':
+      return `${path}jedi-cat.gif`;
+    case 'jedi-cat-02':
+      return `${path}flipflop-cat.gif`;
+    case 'fancy-cat-01':
+      return `${path}dinner-cat.gif`;
+    case 'fancy-cat-02':
+      return `${path}bipedal-cat.gif`;
+      default:
+        return `${path}shaq-cat.gif`;
+  }
+}
