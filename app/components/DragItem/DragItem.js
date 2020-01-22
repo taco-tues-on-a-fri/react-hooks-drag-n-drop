e8547472257de04386fce0bb4604f74329cf5d05
Taco <taco-tues@tuta.io>
2020-01-22 16:17:16 -0700

    Add propTypes

--- app/components/DragItem/DragItem.js
+++ app/components/DragItem/DragItem.js
@@ -1,24 +1,32 @@
 import React, { useState, useRef } from 'react';
 import '../../index.css';
 import DragView from './DragView';
+import PropTypes from 'prop-types';
 import useDrag from '../../hooks/useDrag';
 
 export default function DragItem({ dragEffect, data, id }) {
   const dragRef = useRef();
   const [classValue, setClassValue] = useState('grab');
   
+  // eslint-disable-next-line no-unused-vars
   const { dragState } = useDrag({
     id,
     effect: dragEffect,
     ref: dragRef,
     onDragStart: () => {
-    setClassValue('grabbing')
+      setClassValue('grabbing');
     },
 
     onDragEnd: () => {
-    setClassValue('grab')
+      setClassValue('grab');
     }
   });
   
   return <DragView ref={dragRef} data={data} classValue={classValue} />;
+}
+
+DragItem.propTypes = {
+  dragEffect: PropTypes.string,
+  data: PropTypes.object,
+  id: PropTypes.string
 };
