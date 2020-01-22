7ab855085408fb66f0ce587157604acff10dd249
Taco <taco-tues@tuta.io>
2020-01-22 00:17:33 -0700

    Create DragItem component

---
+++ app/components/DragItem/DragItem.js
@@ -0,0 +1,21 @@
+import React, { useState, useRef } from 'react';
+import '../../index.css';
+import DragView from './DragView';
+import useDrag from '../../hooks/useDrag';
+
+export default function DragItem({ dragEffect, data, id }) {
+  const dragRef = useRef();
+  const [classValue, setClassValue] = useState('grab');
+  
+  const { dragState } = useDrag({
+    id,
+    effect: dragEffect,
+    ref: dragRef,
+    onDragStart: () => setClassValue('grabbing'),
+    onDragEnd: () => {
+      setClassValue('grab');
+    }
+  });
+  
+  return <DragView ref={dragRef} data={data} classValue={classValue} />;
+};
