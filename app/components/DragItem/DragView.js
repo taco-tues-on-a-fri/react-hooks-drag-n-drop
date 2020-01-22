350964d1aced8047f4c0e2246d142a3d2e676d7c
Taco <taco-tues@tuta.io>
2020-01-22 00:12:46 -0700

    Create DragView component

---
+++ app/components/DragItem/DragView.js
@@ -0,0 +1,17 @@
+import React, { forwardRef } from 'react';
+import './index.css';
+import Card from '../Card/Card';
+
+export default forwardRef(({ data, classValue }, ref) => {
+  const { type, title, position } = data;
+  
+  return (
+    <div className={`item ${classValue}`} ref={ref}>
+      <Card
+        type={type}
+        title={title}
+        position={position}
+      />
+    </div>
+  );
+});
