161bdfae366283cdca1b5f6015e7b7441ef7d26a
Taco <taco-tues@tuta.io>
2020-01-22 00:15:50 -0700

    Create useDrop hook

---
+++ app/hooks/useDrop.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+const useDrop = ({ ref, onDrop }) => {
+  const [dropState, updateDropState] = useState('droppable');
+  
+  const dropOverCb = event => {
+    event.preventDefault();
+    updateDropState('dragging over');
+  };
+
+  const dropCb = event => {
+    event.preventDefault();
+    onDrop(ev.dataTransfer.getData('source'));
+    updateDropState('dropped');
+  };
+
+  useEffect(() => {
+    const elem = ref.current;
+    
+    if (elem) {
+      elem.addEventListener('dragover', dropOverCb);
+      elem.addEventListener('drop', dropCb);
+      
+      return () => {
+        elem.removeEventListener('dragover', dropOverCb);
+        elem.removeEventListener('drop', dropCb);
+      };
+    }
+  });
+  
+  return {
+    dropState
+  };
+};
+
+export default useDrop;
