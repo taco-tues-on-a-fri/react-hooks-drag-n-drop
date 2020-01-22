6cd5427ab404721e58117b710ce825b21c27e1f3
Taco <taco-tues@tuta.io>
2020-01-22 00:14:14 -0700

    Create useModal hook

---
+++ app/hooks/useModal.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useRef, useCallback, useState } from "react";
+
+const useModal = () => {
+  const [isShowing, setIsShowing] = useState(false);
+
+  function toggle() {
+    setIsShowing(!isShowing);
+  }
+
+  const handleKeyUp = useCallback(
+    event => {
+      const keys = {
+        27: () => {
+          event.preventDefault();
+          // onCloseRequest();
+          toggle()
+          window.removeEventListener("keyup", handleKeyUp, false);
+        }
+      };
+
+      if (keys[event.keyCode]) {
+        keys[event.keyCode]();
+      }
+    },
+    [toggle]
+  );
+
+  useEffect(() => {
+    if (isShowing === true) {
+    window.addEventListener("keyup", handleKeyUp, false);
+
+    return () => {
+      window.removeEventListener("keyup", handleKeyUp, false);
+    };
+  }}, [handleKeyUp]);
+
+  return {
+    isShowing,
+    toggle,
+  }
+};
+
+export default useModal;
